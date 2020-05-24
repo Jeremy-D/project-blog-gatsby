03a2270ffcfac52e1bb30f29e86b3ec669f98828
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2020-05-24 13:36:24 -0700

    add dummy component so netlify can deploy
    netlify won't deploy empty pages

---
+++ src/pages/test-home.tsx
@@ -0,0 +1,9 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+function Hello(){
+    return <h1>Hello</h1>;
+  }
+
+export default Hello
